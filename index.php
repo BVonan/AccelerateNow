3e6edd37db6dd18d51f1dbe1f192a3af1d180f83
BVonan <bohnny14@gmail.com>
2023-10-04 18:18:41 -0700

    intial setup of the site

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AccelerateNow - Car Rentals</title>
+    <link rel="stylesheet" href="/static/styles.css"> <!-- If you have a separate CSS file, adjust the path accordingly -->
+</head>
+<body>
+    <header>
+        <h1>Welcome to AccelerateNow</h1>
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/cars">Cars</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/contact">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <section id="intro">
+            <h2>Experience the Thrill of Speed</h2>
+            <p>Discover our premium selection of fast cars available for rent.</p>
+            <a href="/cars" class="btn-primary">Explore Cars</a>
+        </section>
+
+        <!-- Additional sections and content can be added here -->
+    </main>
+
+    <footer>
+        <p>&copy; 2023 AccelerateNow</p>
+    </footer>
+</body>
+</html>
