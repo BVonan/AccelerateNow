44eb20735393923b7b22fa958f5ef4fcda36b00b
BVonan <bohnny14@gmail.com>
2023-10-05 11:20:44 -0700

    updated the css to the site

--- index.php
+++ index.php
@@ -3,18 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AccelerateNow - Car Rentals</title>
-    <link rel="stylesheet" href="/static/styles.css"> <!-- If you have a separate CSS file, adjust the path accordingly -->
+    <title>AccelerateNow - Luxury And Super Car Rentals</title>
+    <link rel="stylesheet" href="/assets/css/style.css"> 
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
     <header>
         <h1>Welcome to AccelerateNow</h1>
+        <h2>Luxury And Super Car Rentals</h2>
         <nav>
             <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/cars">Cars</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/contact">Contact</a></li>
+            <?php include 'includes/nav.php'; ?>
             </ul>
         </nav>
     </header>
