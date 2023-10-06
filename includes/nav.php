ee660ff87124f0ca5ed0f81152091c70e0c8c74b
BVonan <bohnny14@gmail.com>
2023-10-06 02:15:17 -0700

    updated the navigataion

--- includes/nav.php
+++ includes/nav.php
@@ -1,7 +1,12 @@
 <!-- navbar.php -->
-<ul class="flex space-x-4 text-white">
-    <li><a href="/" class="hover:text-teal-400">Home</a></li>
-    <li><a href="/cars" class="hover:text-teal-400">Cars</a></li>
-    <li><a href="/about" class="hover:text-teal-400">About</a></li>
-    <li><a href="/contact" class="hover:text-teal-400">Contact</a></li>
+<!-- <link rel="stylesheet" href="/assets/css/style.css"> -->
+
+<!-- HTML -->
+<ul class="flex space-x-4 text-blue">
+    <li class="bold-text"><a href="/" class="hover:text-teal-400">Home</a></li>
+    <li class="bold-text"><a href="/cars" class="hover:text-teal-400">Cars</a></li>
+    <li class="bold-text"><a href="/about" class="hover:text-teal-400">About</a></li>
+    <li class="bold-text"><a href="/ourTeam" class="hover:text-teal-400">Our Team</a></li>
+    <li class="bold-text"><a href="/Testimonials" class="hover:text-teal-400">Testimonials</a></li>
+    <li class="bold-text"><a href="/contact" class="hover:text-teal-400">Contact</a></li>
 </ul>
--- index.php
+++ index.php
@@ -1,65 +1,98 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<!DOCTYPE html>
-<html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AccelerateNow - Luxury And Super Car Rentals</title>
-    <link rel="stylesheet" href="/assets/css/style.css">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <!-- Tailwind CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.7/dist/tailwind.min.css" rel="stylesheet">
-    <!-- Custom CSS -->
-    <link rel="stylesheet" href="/assets/css/custom.css">
-    <!-- JS -->
-    <script src="/assets/js/script.js"></script>
+    <title>AccelerateNow - Luxury And Super Car Rentals<< /title>
+            <link rel="stylesheet" href="/assets/css/style.css">
+            <!-- Bootstrap CSS -->
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+            <!-- Tailwind CSS -->
+            <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.7/dist/tailwind.min.css" rel="stylesheet">
+            <!-- Custom CSS -->
+            <!-- JS -->
+            <script src="/assets/js/script.js"></script>
 </head>
 
-<body class="font-serif bg-light-blue-100">
-    <header class="bg-teal-400 text-white p-4">
-        <h1 class="text-4xl font-extrabold mb-2">Welcome to AccelerateNow</h1>
-        <h2 class="text-xl">Luxury And Super Car Rentals</h2>
-        <nav>
-            <ul class="list-none flex">
-                <?php include 'includes/nav.php'; ?>
-            </ul>
-        </nav>
-    </header>
+<body>
+    <!-- Navigation Bar (Bootstrap) -->
+    <!-- Navigation Bar (Bootstrap) -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <!-- Left Section -->
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">
+                    <img src="fast_car_logo.png" alt="Fast Car Logo" width="30" height="30"
+                        class="d-inline-block align-top">
+                    Accelerate Now
+                </a>
+            </div>
 
-    <main class="container mx-auto py-8">
-        <section id="intro" class="relative bg-gray-800 text-white rounded-lg p-8 mb-8">
-            <div class="absolute inset-0">
-                <div class="flex items-center justify-center h-full">
-                    <!-- Carousel goes here -->
+            <!-- Center Section -->
+            <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+                <?php include 'includes/nav.php'; ?>
+            </div>
+
+            <!-- Right Section -->
+            <div class="flex-none flex items-center justify-center">
+                <div class="flex space-x-4">
+                    <a href="#" class="px-6 h-12 uppercase font-semibold tracking-wider bg-transparent text-black">
+                        Log In
+                    </a>
+                    <a href="#" class="px-6 h-12 uppercase font-semibold tracking-wider bg-blue-500 text-white">
+                        Sign Up
+                    </a>
                 </div>
             </div>
-            <div class="relative z-10">
-                <h2 class="text-4xl font-extrabold mb-4">Experience the Thrill of Speed</h2>
-                <p class="text-lg mb-8">Discover our premium selection of fast cars available for rent.</p>
-                <a href="/cars"
-                    class="bg-red-600 hover:bg-red-700 text-white font-semibold py-2 px-4 rounded-full">Explore Cars</a>
-            </div>
-        </section>
-        <!-- Car Slideshow -->
-        <div class="carousel">
-            <!-- Car images here -->
         </div>
-        <div id="carInfo" class="text-center text-white">
-            <!-- Car information will be displayed here -->
-        </div>
-    </main>
+    </nav>
 
-    <footer class="bg-teal-400 text-white text-center py-4">
-        <p>&copy; 2023 AccelerateNow</p>
+    <!-- Hero Section (Tailwind CSS) -->
+    <header class="bg-gradient-to-r from-blue-500 to-indigo-800 text-white text-center py-20">
+        <h1 class="text-4xl font-semibold">Experience the Ultimate Luxury</h1>
+        <p class="mt-4 text-lg">Rent the finest luxury cars for your special occasions.</p>
+        <a href="#"
+            class="mt-6 inline-block bg-white text-indigo-800 hover:bg-indigo-800 hover:text-white py-2 px-6 rounded-full font-semibold">Explore
+            Cars</a>
+    </header>
+
+    <!-- Car Listing Section (Bootstrap) -->
+    <section class="container mt-5">
+        <h2 class="text-center mb-4">Our Luxury Cars</h2>
+        <div class="row">
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="card">
+                    <img src="car1.jpg" class="card-img-top" alt="Car 1">
+                    <div class="card-body">
+                        <h5 class="card-title">Car Model 1</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <a href="#" class="btn btn-primary">Rent Now</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="card">
+                    <img src="car2.jpg" class="card-img-top" alt="Car 2">
+                    <div class="card-body">
+                        <h5 class="card-title">Car Model 2</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <a href="#" class="btn btn-primary">Rent Now</a>
+                    </div>
+                </div>
+            </div>
+            <!-- Add more car listings here -->
+        </div>
+    </section>
+
+    <!-- Footer (Tailwind CSS) -->
+    <footer class="bg-gray-800 text-white py-6 text-center">
+        <p>&copy; 2023 Luxury Car Rental</p>
     </footer>
+
+    <!-- Bootstrap JS and jQuery (Add these scripts at the end of the body) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 
 </html>
