88c7256aa022432dccfe8bac0a3f8bf4cb101b5c
BVonan <bohnny14@gmail.com>
2023-10-19 10:40:16 -0700

    updated the hrefs in the nav

--- includes/nav.php
+++ includes/nav.php
@@ -4,8 +4,8 @@
 <ul id = "nav" class="flex space-x-4 text-blue">
     <li class="bold-text"><a href="/" class="hover:text-teal-400">Home</a></li>
     <li class="bold-text"><a href="/cars.php" class="hover:text-teal-400">Cars</a></li>
-    <li class="bold-text"><a href="/about" class="hover:text-teal-400">About</a></li>
-    <li class="bold-text"><a href="/ourTeam" class="hover:text-teal-400">Our Team</a></li>
-    <li class="bold-text"><a href="/Testimonials" class="hover:text-teal-400">Testimonials</a></li>
-    <li class="bold-text"><a href="/contact" class="hover:text-teal-400">Contact</a></li>
+    <li class="bold-text"><a href="/about.php" class="hover:text-teal-400">About</a></li>
+    <li class="bold-text"><a href="/ourTeam.php" class="hover:text-teal-400">Our Team</a></li>
+    <li class="bold-text"><a href="/Testimonials.php" class="hover:text-teal-400">Testimonials</a></li>
+    <li class="bold-text"><a href="/contact.php" class="hover:text-teal-400">Contact</a></li>
 </ul>
