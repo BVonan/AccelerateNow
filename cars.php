8a147d3ae29481ba89f327eb2ca6bc514258f15a
BVonan <bohnny14@gmail.com>
2023-10-19 11:45:44 -0700

    added and fixed the login page

--- cars.php
+++ cars.php
@@ -13,26 +13,50 @@
   ```
 -->
 
+<!-- <?php
+// session_start(); // Initialize the session
+?> -->
+
 <!-- header -->
 <?php include 'includes/header.php'; ?>
 
 <!-- nav -->
-<div class="collapse navbar-collapse justify-content-center" id="navbarNav">
-  <?php include 'includes/nav.php'; ?>
-</div>
 
 
-<div class="bg-white">
+    <!-- Navigation Bar (Bootstrap) -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">
+                    <img src="fast_car_logo.png" alt="Fast Car Logo" width="30" height="30"
+                        class="d-inline-block align-top">
+                    Accelerate Now
+                </a>
+            </div>
 
-  <div class="container">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">
-        <img src="fast_car_logo.png" alt="Fast Car Logo" width="30" height="30" class="d-inline-block align-top">
-        Accelerate Now
-      </a>
-      <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
-        <?php include 'includes/nav.php'; ?>
-      </div>
+            <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+                <?php include 'includes/nav.php'; ?>
+            </div>
+
+            <div class="flex-none flex items-center justify-center">
+                <div class="flex space-x-4">
+                    <?php
+                    if (isset($_SESSION['username'])) {
+                        // Display the username and a "Log Out" link if the user is logged in.
+                        echo '<span class="px-6 h-12 uppercase font-semibold tracking-wider bg-transparent text-black">';
+                        echo $_SESSION['username'];
+                        echo '</span>';
+                        echo '<a href="logout.php" class="px-6 h-12 uppercase font-semibold tracking-wider bg-transparent text-black">Log Out</a>';
+                    } else {
+                        // Display "Log In" and "Sign Up" links if the user is not logged in.
+                        echo '<a href="login.php" class="px-6 h-12 uppercase font-semibold tracking-wider bg-transparent text-black">Log In</a>';
+                        echo '<a href="signup.php" class="px-6 h-12 uppercase font-semibold tracking-wider bg-blue-500 text-white">Sign Up</a>';
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </nav>
 
       <div>
         <!--
--- login.php
+++ login.php
@@ -45,26 +45,82 @@ $conn->close();
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <!-- Include necessary meta tags and stylesheets -->
-</head>
+<?php include 'includes/header.php'; ?>
+    <!-- Navigation Bar (Bootstrap) -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">
+                    <img src="fast_car_logo.png" alt="Fast Car Logo" width="30" height="30"
+                        class="d-inline-block align-top">
+                    Accelerate Now
+                </a>
+            </div>
 
+            <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+                <?php include 'includes/nav.php'; ?>
+            </div>
+
+            <div class="flex-none flex items-center justify-center">
+                <div class="flex space-x-4">
+                    <?php
+                    if (isset($_SESSION['username'])) {
+                        // Display the username and a "Log Out" link if the user is logged in.
+                        echo '<span class="px-6 h-12 uppercase font-semibold tracking-wider bg-transparent text-black">';
+                        echo $_SESSION['username'];
+                        echo '</span>';
+                        echo '<a href="logout.php" class="px-6 h-12 uppercase font-semibold tracking-wider bg-transparent text-black">Log Out</a>';
+                    } else {
+                        // Display "Log In" and "Sign Up" links if the user is not logged in.
+                        echo '<a href="login.php" class="px-6 h-12 uppercase font-semibold tracking-wider bg-transparent text-black">Log In</a>';
+                        echo '<a href="signup.php" class="px-6 h-12 uppercase font-semibold tracking-wider bg-blue-500 text-white">Sign Up</a>';
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </nav>
 <body>
-    <h2>Log In</h2>
-    <?php
-    if (isset($loginError)) {
-        echo '<p style="color: red;">' . $loginError . '</p>';
-    }
-    ?>
-    <form action="login.php" method="post">
-        <label for="username">Username:</label>
-        <input type="text" name="username" required><br>
+    <section class="vh-100 gradient-custom">
+        <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                    <div class="card bg-dark text-white" style="border-radius: 1rem;">
+                        <div class="card-body p-5 text-center">
 
-        <label for="password">Password:</label>
-        <input type="password" name="password" required><br>
+                            <div class="mb-md-5 mt-md-4 pb-5">
+                                <h2 class="fw-bold mb-2 text-uppercase">Log In</h2>
+                                <?php
+                                if (isset($loginError)) {
+                                    echo '<p style="color: red;">' . $loginError . '</p>';
+                                }
+                                ?>
+                                <form action="login.php" method="post">
+                                    <div class="form-outline form-white mb-4">
+                                        <label for="username" class="form-label">Username:</label>
+                                        <input type="text" id="typeEmailX" class="form-control form-control-lg"
+                                            name="username" required>
+                                    </div>
+                                    <div class="form-outline form-white mb-4">
+                                        <label for="password" class="form-label">Password:</label>
+                                        <input type="password" id="typePasswordX" class="form-control form-control-lg"
+                                            name="password" required>
+                                    </div>
+                                    <button class="btn btn-outline-light btn-lg px-5" type="submit">Log In</button>
+                                </form>
+                            </div>
 
-        <input type="submit" value="Log In">
-    </form>
+                            <div>
+                                <p class="mb-0">Don't have an account? <a href="#!" class="text-white-50 fw-bold">Sign
+                                        Up</a></p>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </body>
 
 </html>
