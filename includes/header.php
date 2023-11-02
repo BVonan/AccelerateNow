4ebea79af9a45632dc77a38d449f12e378c54205
BVonan <bohnny14@gmail.com>
2023-11-02 13:50:52 -0700

    updated the cars.php but not done

--- includes/header.php
+++ includes/header.php
@@ -11,4 +11,6 @@
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.7/dist/tailwind.min.css" rel="stylesheet">
     <link rel="stylesheet" href="/public/css/tailwind.css">
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@2.3.0/dist/alpine.js" defer></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.7/dist/tailwind.min.css">
 </head>
