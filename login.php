a788ecca97fb9b8ec74291df6e3de8bb3c133d6a
BVonan <bohnny14@gmail.com>
2023-11-02 12:51:25 -0700

    fixed the sql connection for the log in

--- login.php
+++ login.php
@@ -17,7 +17,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $enteredPassword = $_POST["password"];
 
     // Fetch user's data from the database based on the entered username.
-    $query = "SELECT id, username, password FROM users WHERE username = ?";
+    $query = "SELECT user_id, username, password FROM users WHERE username = ?";
     $stmt = $conn->prepare($query);
     $stmt->bind_param("s", $enteredUsername);
     $stmt->execute();
@@ -46,40 +46,41 @@ $conn->close();
 <html lang="en">
 
 <?php include 'includes/header.php'; ?>
-    <!-- Navigation Bar (Bootstrap) -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="#">
-                    <img src="fast_car_logo.png" alt="Fast Car Logo" width="30" height="30"
-                        class="d-inline-block align-top">
-                    Accelerate Now
-                </a>
-            </div>
+<!-- Navigation Bar (Bootstrap) -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">
+                <img src="fast_car_logo.png" alt="Fast Car Logo" width="30" height="30"
+                    class="d-inline-block align-top">
+                Accelerate Now
+            </a>
+        </div>
 
-            <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
-                <?php include 'includes/nav.php'; ?>
-            </div>
+        <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+            <?php include 'includes/nav.php'; ?>
+        </div>
 
-            <div class="flex-none flex items-center justify-center">
-                <div class="flex space-x-4">
-                    <?php
-                    if (isset($_SESSION['username'])) {
-                        // Display the username and a "Log Out" link if the user is logged in.
-                        echo '<span class="px-6 h-12 uppercase font-semibold tracking-wider bg-transparent text-black">';
-                        echo $_SESSION['username'];
-                        echo '</span>';
-                        echo '<a href="logout.php" class="px-6 h-12 uppercase font-semibold tracking-wider bg-transparent text-black">Log Out</a>';
-                    } else {
-                        // Display "Log In" and "Sign Up" links if the user is not logged in.
-                        echo '<a href="login.php" class="px-6 h-12 uppercase font-semibold tracking-wider bg-transparent text-black">Log In</a>';
-                        echo '<a href="signup.php" class="px-6 h-12 uppercase font-semibold tracking-wider bg-blue-500 text-white">Sign Up</a>';
-                    }
-                    ?>
-                </div>
+        <div class="flex-none flex items-center justify-center">
+            <div class="flex space-x-4">
+                <?php
+                if (isset($_SESSION['username'])) {
+                    // Display the username and a "Log Out" link if the user is logged in.
+                    echo '<span class="px-6 h-12 uppercase font-semibold tracking-wider bg-transparent text-black">';
+                    echo $_SESSION['username'];
+                    echo '</span>';
+                    echo '<a href="logout.php" class="px-6 h-12 uppercase font-semibold tracking-wider bg-transparent text-black">Log Out</a>';
+                } else {
+                    // Display "Log In" and "Sign Up" links if the user is not logged in.
+                    echo '<a href="login.php" class="px-6 h-12 uppercase font-semibold tracking-wider bg-transparent text-black">Log In</a>';
+                    echo '<a href="signup.php" class="px-6 h-12 uppercase font-semibold tracking-wider bg-blue-500 text-white">Sign Up</a>';
+                }
+                ?>
             </div>
         </div>
-    </nav>
+    </div>
+</nav>
+
 <body>
     <section class="vh-100 gradient-custom">
         <div class="container py-5 h-100">
