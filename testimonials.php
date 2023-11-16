dec02cfb87c7d254d17b9d531596bda05934e71e
BVonan <bohnny14@gmail.com>
2023-11-15 18:26:02 -0800

    updated the testimonials page

--- testimonials.php
+++ testimonials.php
@@ -1,93 +1,86 @@
 <?php
 session_start();
 
-// Your database connection and login logic here...
+// Replace with your database credentials
+$host = "localhost:3306";
+$username = "root";
+$password = "bohnnY06!";
+$dbName = "AccelerateNow";
 
-// Redirect if the user is already logged in
-if (isset($_SESSION['username'])) {
-    header("Location: index.php"); // Redirect to the homepage or dashboard
+// Establish a connection to the database
+$conn = new mysqli($host, $username, $password, $dbName);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Redirect if the user is not logged in
+if (!isset($_SESSION['username'])) {
+    header("Location: login.php"); // Redirect to the login page
     exit();
+}
+
+// Handling form submission to add testimonials
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $customerName = $_SESSION['username'];
+    $testimonialText = $_POST['testimonial_text'];
+    $rating = $_POST['rating'];
+
+    // Perform validation if needed
+
+    // Insert the testimonial into the database
+    $insertQuery = "INSERT INTO Testimonials (CustomerName, TestimonialText, Rating, TestimonialDate) VALUES (?, ?, ?, NOW())";
+    $stmt = $conn->prepare($insertQuery);
+    $stmt->bind_param("ssi", $customerName, $testimonialText, $rating);
+    $stmt->execute();
+    $stmt->close();
+    // Optionally, redirect the user to a success page or perform other actions after adding the testimonial
 }
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+<head>
+    <!-- Include necessary meta tags, CSS, and scripts -->
+</head>
+<body>
 
 <?php include 'includes/header.php'; ?>
 <!-- Navigation Bar (Bootstrap) -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container">
-        <div class="navbar-header">
-            <a class="navbar-brand" href="#">
-                <img src="fast_car_logo.png" alt="Fast Car Logo" width="30" height="30"
-                    class="d-inline-block align-top">
-                Accelerate Now
-            </a>
-        </div>
+<!-- Your navigation bar code here -->
 
-        <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
-            <?php include 'includes/nav.php'; ?>
-        </div>
+<section class="vh-100 gradient-custom">
+    <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                <div class="card bg-dark text-white" style="border-radius: 1rem;">
+                    <div class="card-body p-5 text-center">
 
-        <div class="flex-none flex items-center justify-center">
-            <div class="flex space-x-4">
-                <?php
-                if (isset($_SESSION['username'])) {
-                    // Display the username and a "Log Out" link if the user is logged in.
-                    echo '<span class="px-6 h-12 uppercase font-semibold tracking-wider bg-transparent text-black">';
-                    echo $_SESSION['username'];
-                    echo '</span>';
-                    echo '<a href="logout.php" class="px-6 h-12 uppercase font-semibold tracking-wider bg-transparent text-black">Log Out</a>';
-                } else {
-                    // Display "Log In" and "Sign Up" links if the user is not logged in.
-                    echo '<a href="login.php" class="px-6 h-12 uppercase font-semibold tracking-wider bg-transparent text-black">Log In</a>';
-                    echo '<a href="signup.php" class="px-6 h-12 uppercase font-semibold tracking-wider bg-blue-500 text-white">Sign Up</a>';
-                }
-                ?>
-            </div>
-        </div>
-    </div>
-</nav>
-
-<body>
-
-    <?php include 'includes/header.php'; ?>
-    <!-- Navigation Bar (Bootstrap) -->
-    <!-- Your navigation bar code here -->
-
-    <section class="vh-100 gradient-custom">
-        <div class="container py-5 h-100">
-            <div class="row d-flex justify-content-center align-items-center h-100">
-                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-                    <div class="card bg-dark text-white" style="border-radius: 1rem;">
-                        <div class="card-body p-5 text-center">
-
-                            <div class="mb-md-5 mt-md-4 pb-5">
-                                <a href="login.php" class="btn btn-primary fw-bold mb-2 text-uppercase">Log In</a>
-                                <?php
-                                if (isset($loginError)) {
-                                    echo '<p style="color: red;">' . $loginError . '</p>';
-                                }
-                                ?>
-                                <!-- Your login form fields -->
-                            </div>
-
-
-                            <div>
-                                <p class="mb-0">Don't have an account? <a href="signup.php"
-                                        class="text-white-50 fw-bold">Sign
-                                        Up</a></p>
-                            </div>
-
+                        <div class="mb-md-5 mt-md-4 pb-5">
+                            <h2 class="fw-bold mb-2 text-uppercase">Create Testimonial</h2>
+                            <form action="testimonials.php" method="post">
+                                <div class="mb-3">
+                                    <label for="testimonial_text" class="form-label">Your Testimonial:</label>
+                                    <textarea class="form-control" id="testimonial_text" name="testimonial_text" rows="4" required></textarea>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="rating" class="form-label">Rating (1-5):</label>
+                                    <input type="number" class="form-control" id="rating" name="rating" min="1" max="5" required>
+                                </div>
+                                <button type="submit" class="btn btn-primary">Submit Testimonial</button>
+                            </form>
                         </div>
+
+                        <!-- Other content -->
+
                     </div>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 
-    <!-- Include necessary scripts -->
+<!-- Include necessary scripts -->
 </body>
-
 </html>
