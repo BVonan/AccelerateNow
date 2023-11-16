ad1dec79dd8c07144445f78467ae531bc04ea1d2
BVonan <bohnny14@gmail.com>
2023-11-15 18:10:55 -0800

    updated css

--- about.php
+++ about.php
@@ -54,22 +54,22 @@
 
     <div class="row">
         <div class="col-md-6">
-            <h2>Our Passion for Exclusivity</h2>
-            <p>Accelerate Now was founded on our deep passion for the world's most exclusive and high-performance automobiles. We aim to provide an unparalleled experience that merges luxury, performance, and exceptional service.</p>
+            <h2 class="about">Our Passion for Exclusivity</h2>
+            <p class="aboutp">Accelerate Now was founded on our deep passion for the world's most exclusive and high-performance automobiles. We aim to provide an unparalleled experience that merges luxury, performance, and exceptional service.</p>
 
-            <h2>Unrivaled Fleet of Luxury and Hyper Cars</h2>
-            <p>Our curated collection includes iconic vehicles from Lamborghini, Ferrari, Porsche, McLaren, and more. Each car in our fleet embodies excellence in engineering and design, promising an extraordinary driving experience.</p>
+            <h2 class="about">Unrivaled Fleet of Luxury and Hyper Cars</h2>
+            <p class="aboutp">Our curated collection includes iconic vehicles from Lamborghini, Ferrari, Porsche, McLaren, and more. Each car in our fleet embodies excellence in engineering and design, promising an extraordinary driving experience.</p>
         </div>
 
         <div class="col-md-6">
-            <h2>Seamless Rental Experience</h2>
-            <p>With Accelerate Now, your rental experience is effortless. Our user-friendly online platform coupled with our dedicated team ensures a smooth process from car selection to return, focusing on making your journey memorable.</p>
+            <h2 class="about">Seamless Rental Experience</h2>
+            <p class="aboutp">With Accelerate Now, your rental experience is effortless. Our user-friendly online platform coupled with our dedicated team ensures a smooth process from car selection to return, focusing on making your journey memorable.</p>
 
-            <h2>Passionate and Knowledgeable Team</h2>
-            <p>Our team comprises passionate individuals dedicated to providing exceptional service. We're here to guide you through the rental process, offering expert advice and personalized recommendations.</p>
+            <h2 class="about">Passionate and Knowledgeable Team</h2>
+            <p class="aboutp"> "Our team comprises passionate individuals dedicated to providing exceptional service. We're here to guide you through the rental process, offering expert advice and personalized recommendations.</p>
 
-            <h2>Your Adventure Awaits</h2>
-            <p>Embark on an exhilarating journey with Accelerate Now and experience the thrill of driving the world's most coveted automobiles.</p>
+            <h2 class="about">Your Adventure Awaits</h2>
+            <p class="aboutp">Embark on an exhilarating journey with Accelerate Now and experience the thrill of driving the world's most coveted automobiles.</p>
         </div>
     </div>
 </div>
