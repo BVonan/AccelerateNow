29ab2ec252cff2d529297a8154997cd3ca435527
BVonan <bohnny14@gmail.com>
2023-11-24 15:30:52 -0800

    added a little to the body

---
+++ contact.php
@@ -0,0 +1,16 @@
+<?php
+session_start(); // Initialize the session
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Home</title>
+</head>
+
+<?php include 'includes/header.php'; ?>
+
+
+<body>
+</body>
