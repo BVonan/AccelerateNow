a92fd9fd40816cbfcda0a891a1c23a69cf93e49e
BVonan <bohnny14@gmail.com>
2023-11-29 14:36:36 -0800

    added the csv and sql file to the code

---
+++ AccelerateNow.sql
@@ -0,0 +1,199 @@
+SELECT * FROM AccelerateNow.cars;
+-- Car 1
+INSERT INTO cars (image, year, name, company, engine_type, zero_to_sixty, mpg, fuel_type, seating_space, seats, traveling_capacity, cost_per_day)
+VALUES ('car_image1.jpg', 2023, 'BMW M8', 'BMW', 'V8', '3.2 seconds', '24 MPG', 'Gasoline', '4 seats', 4, '350 miles', 350.00);
+
+-- Car 2
+INSERT INTO cars (image, year, name, company, engine_type, zero_to_sixty, mpg, fuel_type, seating_space, seats, traveling_capacity, cost_per_day)
+VALUES ('car_image2.jpg', 2023, 'Audi RS7', 'Audi', 'V8', '3.5 seconds', '23 MPG', 'Gasoline', '5 seats', 5, '320 miles', 330.00);
+
+-- Car 3
+INSERT INTO cars (image, year, name, company, engine_type, zero_to_sixty, mpg, fuel_type, seating_space, seats, traveling_capacity, cost_per_day)
+VALUES ('car_image3.jpg', 2023, 'Lexus LC 500', 'Lexus', 'V8', '4.4 seconds', '25 MPG', 'Gasoline', '4 seats', 4, '310 miles', 300.00);
+
+-- Car 4
+INSERT INTO cars (image, year, name, company, engine_type, zero_to_sixty, mpg, fuel_type, seating_space, seats, traveling_capacity, cost_per_day)
+VALUES ('car_image4.jpg', 2023, 'Porsche 911 Turbo S', 'Porsche', 'Flat-6', '2.6 seconds', '21 MPG', 'Gasoline', '2 seats', 2, '280 miles', 400.00);
+
+-- Car 5
+INSERT INTO cars (image, year, name, company, engine_type, zero_to_sixty, mpg, fuel_type, seating_space, seats, traveling_capacity, cost_per_day)
+VALUES ('car_image5.jpg', 2023, 'Tesla Model S Plaid', 'Tesla', 'Electric', '1.9 seconds', '350 miles', 'Electric', '5 seats', 5, '350 miles', 450.00);
+
+-- Car 6
+INSERT INTO cars (image, year, name, company, engine_type, zero_to_sixty, mpg, fuel_type, seating_space, seats, traveling_capacity, cost_per_day)
+VALUES ('car_image6.jpg', 2023, 'McLaren 720S', 'McLaren', 'V8', '2.8 seconds', '20 MPG', 'Gasoline', '2 seats', 2, '250 miles', 500.00);
+
+-- Car 7
+INSERT INTO cars (image, year, name, company, engine_type, zero_to_sixty, mpg, fuel_type, seating_space, seats, traveling_capacity, cost_per_day)
+VALUES ('car_image7.jpg', 2023, 'Bugatti Chiron', 'Bugatti', 'W16', '2.5 seconds', '9 MPG', 'Gasoline', '2 seats', 2, '220 miles', 550.00);
+
+-- Car 8
+INSERT INTO cars (image, year, name, company, engine_type, zero_to_sixty, mpg, fuel_type, seating_space, seats, traveling_capacity, cost_per_day)
+VALUES ('car_image8.jpg', 2023, 'Rolls-Royce Phantom', 'Rolls-Royce', 'V12', '5.1 seconds', '19 MPG', 'Gasoline', '5 seats', 5, '350 miles', 600.00);
+
+-- Car 9
+INSERT INTO cars (image, year, name, company, engine_type, zero_to_sixty, mpg, fuel_type, seating_space, seats, traveling_capacity, cost_per_day)
+VALUES ('car_image9.jpg', 2023, 'Lamborghini Huracán', 'Lamborghini', 'V10', '2.9 seconds', '14 MPG', 'Gasoline', '2 seats', 2, '220 miles', 650.00);
+
+-- Car 10
+INSERT INTO cars (image, year, name, company, engine_type, zero_to_sixty, mpg, fuel_type, seating_space, seats, traveling_capacity, cost_per_day)
+VALUES ('car_image10.jpg', 2023, 'Jaguar F-Type', 'Jaguar', 'V6', '3.5 seconds', '24 MPG', 'Gasoline', '2 seats', 2, '260 miles', 300.00);
+
+-- Car 11
+INSERT INTO cars (image, year, name, company, engine_type, zero_to_sixty, mpg, fuel_type, seating_space, seats, traveling_capacity, cost_per_day)
+VALUES ('car_image11.jpg', 2023, 'Aston Martin DB11', 'Aston Martin', 'V8', '4.0 seconds', '22 MPG', 'Gasoline', '4 seats', 4, '300 miles', 330.00);
+
+-- Car 12
+INSERT INTO cars (image, year, name, company, engine_type, zero_to_sixty, mpg, fuel_type, seating_space, seats, traveling_capacity, cost_per_day)
+VALUES ('car_image12.jpg', 2023, 'Ferrari 488 GTB', 'Ferrari', 'V8', '2.8 seconds', '18 MPG', 'Gasoline', '2 seats', 2, '250 miles', 500.00);
+
+-- Car 13
+INSERT INTO cars (image, year, name, company, engine_type, zero_to_sixty, mpg, fuel_type, seating_space, seats, traveling_capacity, cost_per_day)
+VALUES ('car_image13.jpg', 2023, 'Bentley Continental GT', 'Bentley', 'W12', '3.4 seconds', '19 MPG', 'Gasoline', '4 seats', 4, '300 miles', 400.00);
+
+-- Car 14
+INSERT INTO cars (image, year, name, company, engine_type, zero_to_sixty, mpg, fuel_type, seating_space, seats, traveling_capacity, cost_per_day)
+VALUES ('car_image14.jpg', 2023, 'Koenigsegg Jesko', 'Koenigsegg', 'V8', '2.5 seconds', '15 MPG', 'Gasoline', '2 seats', 2, '240 miles', 700.00);
+
+-- Car 15
+INSERT INTO cars (image, year, name, company, engine_type, zero_to_sixty, mpg, fuel_type, seating_space, seats, traveling_capacity, cost_per_day)
+VALUES ('car_image15.jpg', 2023, 'Pagani Huayra', 'Pagani', 'V12', '3.2 seconds', '14 MPG', 'Gasoline', '2 seats', 2, '230 miles', 600.00);
+-- Continue with the rest of the cars
+
+-- Car 16
+INSERT INTO cars (image, year, name, company, engine_type, zero_to_sixty, mpg, fuel_type, seating_space, seats, traveling_capacity, cost_per_day)
+VALUES ('car_image16.jpg', 2023, 'Volvo S60', 'Volvo', 'Inline-4', '5.7 seconds', '30 MPG', 'Gasoline', '5 seats', 5, '400 miles', 200.00);
+
+-- Car 17
+INSERT INTO cars (image, year, name, company, engine_type, zero_to_sixty, mpg, fuel_type, seating_space, seats, traveling_capacity, cost_per_day)
+VALUES ('car_image17.jpg', 2023, 'Volkswagen Golf R', 'Volkswagen', 'Inline-4', '4.8 seconds', '28 MPG', 'Gasoline', '5 seats', 5, '380 miles', 250.00);
+
+-- Car 18
+INSERT INTO cars (image, year, name, company, engine_type, zero_to_sixty, mpg, fuel_type, seating_space, seats, traveling_capacity, cost_per_day)
+VALUES ('car_image18.jpg', 2023, 'Tesla Model X', 'Tesla', 'Electric', '3.8 seconds', '300 miles', 'Electric', '7 seats', 7, '300 miles', 400.00);
+
+-- Car 19
+INSERT INTO cars (image, year, name, company, engine_type, zero_to_sixty, mpg, fuel_type, seating_space, seats, traveling_capacity, cost_per_day)
+VALUES ('car_image19.jpg', 2023, 'Audi R8', 'Audi', 'V10', '3.2 seconds', '17 MPG', 'Gasoline', '2 seats', 2, '220 miles', 400.00);
+
+-- Car 20
+INSERT INTO cars (image, year, name, company, engine_type, zero_to_sixty, mpg, fuel_type, seating_space, seats, traveling_capacity, cost_per_day)
+VALUES ('car_image20.jpg', 2023, 'BMW i8', 'BMW', 'Hybrid', '4.2 seconds', '28 MPG', 'Gasoline', '2 seats', 2, '250 miles', 300.00);
+
+-- Car 21
+INSERT INTO cars (image, year, name, company, engine_type, zero_to_sixty, mpg, fuel_type, seating_space, seats, traveling_capacity, cost_per_day)
+VALUES ('car_image21.jpg', 2023, 'Ferrari SF90 Stradale', 'Ferrari', 'Hybrid', '2.5 seconds', '15 MPG', 'Gasoline', '2 seats', 2, '230 miles', 600.00);
+
+-- Car 22
+INSERT INTO cars (image, year, name, company, engine_type, zero_to_sixty, mpg, fuel_type, seating_space, seats, traveling_capacity, cost_per_day)
+VALUES ('car_image22.jpg', 2023, 'Lamborghini Aventador', 'Lamborghini', 'V12', '2.7 seconds', '11 MPG', 'Gasoline', '2 seats', 2, '200 miles', 550.00);
+
+-- Car 23
+INSERT INTO cars (image, year, name, company, engine_type, zero_to_sixty, mpg, fuel_type, seating_space, seats, traveling_capacity, cost_per_day)
+VALUES ('car_image23.jpg', 2023, 'Porsche Taycan Turbo S', 'Porsche', 'Electric', '2.6 seconds', '270 miles', 'Electric', '4 seats', 4, '270 miles', 450.00);
+
+-- Car 24
+INSERT INTO cars (image, year, name, company, engine_type, zero_to_sixty, mpg, fuel_type, seating_space, seats, traveling_capacity, cost_per_day)
+VALUES ('car_image24.jpg', 2023, 'McLaren GT', 'McLaren', 'V8', '3.2 seconds', '19 MPG', 'Gasoline', '2 seats', 2, '250 miles', 500.00);
+
+-- Car 25
+INSERT INTO cars (image, year, name, company, engine_type, zero_to_sixty, mpg, fuel_type, seating_space, seats, traveling_capacity, cost_per_day)
+VALUES ('car_image25.jpg', 2023, 'Bugatti Veyron', 'Bugatti', 'W16', '2.6 seconds', '10 MPG', 'Gasoline', '2 seats', 2, '220 miles', 600.00);
+
+
+
+-- Add the 'fuel_type' column to the 'cars' table
+ALTER TABLE cars
+ADD COLUMN fuel_type VARCHAR(50);
+
+-- Update the 'fuel_type' for each car
+UPDATE cars SET fuel_type = 'Gasoline' WHERE id IN (1, 2, 3, 4, 10, 11, 13, 16, 17, 19, 20, 24, 31, 33, 37, 38, 40, 42, 44, 46, 48, 51, 54, 55);
+UPDATE cars SET fuel_type = 'Electric' WHERE id IN (5, 18, 23, 57);
+UPDATE cars SET fuel_type = 'Hybrid' WHERE id IN (20, 21, 31);
+UPDATE cars SET fuel_type = 'Diesel' WHERE id = 9;
+UPDATE cars SET fuel_type = 'Biofuel' WHERE id = 28;
+UPDATE cars SET fuel_type = 'Hydrogen' WHERE id = 34;
+UPDATE cars SET fuel_type = 'E85' WHERE id = 50;
+UPDATE cars SET fuel_type = 'CNG' WHERE id = 55;
+UPDATE cars SET fuel_type = 'Propane' WHERE id = 60;
+-- Continue to set the 'fuel_type' for the remaining cars
+
+
+
+-- Doesnt work down below
+
+INSERT INTO cars (image, year, name, company, engine_type, zero_to_sixty, mpg, fuel_type, seating_space, seats, traveling_capacity, cost_per_day)
+VALUES
+  ('mercedes_s_class.jpg', 2023, 'Mercedes-Benz S-Class', 'Mercedes-Benz', 'V8', '4.4 seconds', 27, 'Gasoline', '5 seats', 5, '500 miles', 250.00),
+  ('bmw_7_series.jpg', 2023, 'BMW 7 Series', 'BMW', 'Inline-6', '4.3 seconds', 29, 'Gasoline', '5 seats', 5, '480 miles', 240.00),
+  ('audi_a8.jpg', 2023, 'Audi A8', 'Audi', 'V6', '4.8 seconds', 27, 'Gasoline', '5 seats', 5, '460 miles', 230.00),
+  ('lexus_ls.jpg', 2023, 'Lexus LS', 'Lexus', 'V6', '5.1 seconds', 25, 'Gasoline', '5 seats', 5, '440 miles', 220.00),
+  ('porsche_panamera.jpg', 2023, 'Porsche Panamera', 'Porsche', 'V6', '3.5 seconds', 23, 'Gasoline', '4 seats', 4, '420 miles', 300.00),
+  ('tesla_model_s.jpg', 2023, 'Tesla Model S', 'Tesla', 'Electric', '2.3 seconds', 120, 'Electric', '5 seats', 5, '350 miles', 300.00),
+  ('porsche_taycan.jpg', 2023, 'Porsche Taycan', 'Porsche', 'Electric', '2.6 seconds', 100, 'Electric', '4 seats', 4, '320 miles', 280.00),
+  ('mclaren_p1.jpg', 2023, 'McLaren P1', 'McLaren', 'Hybrid', '2.7 seconds', 18, 'Hybrid', '2 seats', 2, '250 miles', 500.00),
+  ('bugatti_chiron.jpg', 2023, 'Bugatti Chiron', 'Bugatti', 'Gasoline', '2.5 seconds', 9, 'Gasoline', '2 seats', 2, '200 miles', 1000.00),
+  ('rolls_royce_ghost.jpg', 2023, 'Rolls-Royce Ghost', 'Rolls-Royce', 'V12', '4.8 seconds', 15, 'Gasoline', '5 seats', 5, '350 miles', 400.00),
+  ('bmw_i8.jpg', 2023, 'BMW i8', 'BMW', 'Hybrid', '4.2 seconds', 28, 'Hybrid', '2 seats', 2, '350 miles', 250.00),
+  ('lamborghini_veneno.jpg', 2023, 'Lamborghini Veneno', 'Lamborghini', 'Gasoline', '2.8 seconds', 10, 'Gasoline', '2 seats', 2, '180 miles', 750.00),
+  ('audi_e-tron.jpg', 2023, 'Audi e-tron', 'Audi', 'Electric', '3.9 seconds', 80, 'Electric', '5 seats', 5, '280 miles', 280.00),
+  ('jaguar_i-pace.jpg', 2023, 'Jaguar I-PACE', 'Jaguar', 'Electric', '4.5 seconds', 76, 'Electric', '5 seats', 5, '300 miles', 290.00),
+  ('mercedes_amg_gtr.jpg', 2023, 'Mercedes-AMG GT R', 'Mercedes-Benz', 'V8', '3.5 seconds', 15, 'Gasoline', '2 seats', 2, '220 miles', 400.00),
+  ('aston_martin_vanquish.jpg', 2023, 'Aston Martin Vanquish', 'Aston Martin', 'V12', '3.2 seconds', 15, 'Gasoline', '2 seats', 2, '240 miles', 450.00),
+  ('ferrari_812_superfast.jpg', 2023, 'Ferrari 812 Superfast', 'Ferrari', 'V12', '2.9 seconds', 12, 'Gasoline', '2 seats', 2, '250 miles', 500.00),
+  ('porsche_911_turbo.jpg', 2023, 'Porsche 911 Turbo', 'Porsche', 'Flat-6', '2.7 seconds', 21, 'Gasoline', '4 seats', 4, '320 miles', 300.00),
+  ('bentley_continental_gt.jpg', 2023, 'Bentley Continental GT', 'Bentley', 'W12', '3.6 seconds', 17, 'Gasoline', '4 seats', 4, '320 miles', 350.00),
+  ('koenigsegg_agera.jpg', 2023, 'Koenigsegg Agera', 'Koenigsegg', 'V8', '2.9 seconds', 15, 'Gasoline', '2 seats', 2, '220 miles', 750.00),
+  ('lexus_lc.jpg', 2023, 'Lexus LC', 'Lexus', 'V8', '4.4 seconds', 24, 'Gasoline', '4 seats', 4, '300 miles', 300.00),
+  ('audi_r8.jpg', 2023, 'Audi R8', 'Audi', 'V10', '3.1 seconds', 18, 'Gasoline', '2 seats', 2, '240 miles', 400.00),
+  ('porsche_cayman.jpg', 2023, 'Porsche 718 Cayman', 'Porsche', 'Flat-4', '4.9 seconds', 24, 'Gasoline', '2 seats', 2, '280 miles', 280.00),
+  ('mclaren_720s.jpg', 2023, 'McLaren 720S', 'McLaren', 'V8', '2.8 seconds', 18, 'Gasoline', '2 seats', 2, '220 miles', 500.00),
+  ('bugatti_veyron.jpg', 2023, 'Bugatti Veyron', 'Bugatti', 'Gasoline', '2.5 seconds', 10, 'Gasoline', '2 seats', 2, '180 miles', 1000.00);
+
+INSERT INTO cars (image, year, name, company, engine_type, zero_to_sixty, mpg, fuel_type, seating_space, seats, traveling_capacity, cost_per_day)
+VALUES
+  ('rolls_royce_wraith.jpg', 2023, 'Rolls-Royce Wraith', 'Rolls-Royce', 'V12', '4.4 seconds', 15, 'Gasoline', '4 seats', 4, '350 miles', 400.00),
+  ('mclaren_senna.jpg', 2023, 'McLaren Senna', 'McLaren', 'V8', '2.8 seconds', 18, 'Gasoline', '2 seats', 2, '220 miles', 500.00),
+  ('lamborghini_huracan.jpg', 2023, 'Lamborghini Huracan', 'Lamborghini', 'V10', '2.9 seconds', 14, 'Gasoline', '2 seats', 2, '220 miles', 400.00),
+  ('bentley_flying_spur.jpg', 2023, 'Bentley Flying Spur', 'Bentley', 'V8', '3.8 seconds', 20, 'Gasoline', '5 seats', 5, '340 miles', 350.00),
+  ('pagani_huayra.jpg', 2023, 'Pagani Huayra', 'Pagani', 'V12', '2.8 seconds', 11, 'Gasoline', '2 seats', 2, '250 miles', 750.00),
+  ('audi_rs7.jpg', 2023, 'Audi RS7', 'Audi', 'V8', '3.4 seconds', 22, 'Gasoline', '5 seats', 5, '320 miles', 350.00),
+  ('aston_martin_db11.jpg', 2023, 'Aston Martin DB11', 'Aston Martin', 'V8', '3.6 seconds', 18, 'Gasoline', '4 seats', 4, '320 miles', 300.00),
+  ('ferrari_f8_tributo.jpg', 2023, 'Ferrari F8 Tributo', 'Ferrari', 'V8', '2.9 seconds', 16, 'Gasoline', '2 seats', 2, '240 miles', 500.00),
+  ('porsche_panamera_turbo.jpg', 2023, 'Porsche Panamera Turbo', 'Porsche', 'V8', '3.3 seconds', 22, 'Gasoline', '4 seats', 4, '320 miles', 350.00),
+  ('rolls_royce_cullinan.jpg', 2023, 'Rolls-Royce Cullinan', 'Rolls-Royce', 'V12', '4.8 seconds', 12, 'Gasoline', '5 seats', 5, '340 miles', 400.00),
+  ('bugatti_divo.jpg', 2023, 'Bugatti Divo', 'Bugatti', 'W16', '2.4 seconds', 8, 'Gasoline', '2 seats', 2, '250 miles', 1000.00),
+  ('lamborghini_urus.jpg', 2023, 'Lamborghini Urus', 'Lamborghini', 'V8', '3.6 seconds', 14, 'Gasoline', '5 seats', 5, '300 miles', 400.00),
+  ('bentley_bentayga.jpg', 2023, 'Bentley Bentayga', 'Bentley', 'V8', '4.5 seconds', 16, 'Gasoline', '5 seats', 5, '320 miles', 350.00),
+  ('mclaren_gt.jpg', 2023, 'McLaren GT', 'McLaren', 'V8', '3.2 seconds', 20, 'Gasoline', '2 seats', 2, '240 miles', 450.00),
+  ('aston_martin_vantage.jpg', 2023, 'Aston Martin Vantage', 'Aston Martin', 'V8', '3.5 seconds', 18, 'Gasoline', '2 seats', 2, '260 miles', 300.00),
+  ('ferrari_portofino.jpg', 2023, 'Ferrari Portofino', 'Ferrari', 'V8', '3.2 seconds', 20, 'Gasoline', '2 seats', 2, '250 miles', 350.00),
+  ('porsche_911_gt3.jpg', 2023, 'Porsche 911 GT3', 'Porsche', 'Flat-6', '3.2 seconds', 17, 'Gasoline', '2 seats', 2, '280 miles', 400.00),
+  ('rolls_royce_dawn.jpg', 2023, 'Rolls-Royce Dawn', 'Rolls-Royce', 'V12', '4.4 seconds', 14, 'Gasoline', '4 seats', 4, '300 miles', 450.00),
+  ('koenigsegg_regera.jpg', 2023, 'Koenigsegg Regera', 'Koenigsegg', 'V8', '2.7 seconds', 12, 'Gasoline', '2 seats', 2, '220 miles', 1000.00),
+  ('lexus_ux.jpg', 2023, 'Lexus UX', 'Lexus', 'Hybrid', '7.3 seconds', 42, 'Hybrid', '5 seats', 5, '450 miles', 200.00),
+  ('audi_q7.jpg', 2023, 'Audi Q7', 'Audi', 'V6', '5.6 seconds', 22, 'Gasoline', '7 seats', 7, '360 miles', 230.00),
+  ('bmw_x5.jpg', 2023, 'BMW X5', 'BMW', 'Inline-6', '5.3 seconds', 21, 'Gasoline', '5 seats', 5, '340 miles', 220.00),
+  ('volvo_xc90.jpg', 2023, 'Volvo XC90', 'Volvo', 'Inline-4', '6.1 seconds', 25, 'Gasoline', '7 seats', 7, '380 miles', 210.00),
+  ('mercedes_gls.jpg', 2023, 'Mercedes-Benz GLS', 'Mercedes-Benz', 'V8', '4.8 seconds', 18, 'Gasoline', '7 seats', 7, '350 miles', 240.00),
+  ('audi_q5.jpg', 2023, 'Audi Q5', 'Audi', 'Inline-4', '6.2 seconds', 28, 'Gasoline', '5 seats', 5, '380 miles', 220.00),
+  ('lexus_nx.jpg', 2023, 'Lexus NX', 'Lexus', 'Inline-4', '7.0 seconds', 30, 'Hybrid', '5 seats', 5, '400 miles', 210.00),
+  ('tesla_model_y.jpg', 2023, 'Tesla Model Y', 'Tesla', 'Electric', '3.9 seconds', 120, 'Electric', '5 seats', 5, '350 miles', 250.00),
+  ('volkswagen_touareg.jpg', 2023, 'Volkswagen Touareg', 'Volkswagen', 'V6', '6.2 seconds', 24, 'Diesel', '5 seats', 5, '380 miles', 220.00),
+  ('jaguar_f-pace.jpg', 2023, 'Jaguar F-PACE', 'Jaguar', 'Inline-4', '6.4 seconds', 30, 'Gasoline', '5 seats', 5, '400 miles', 230.00),
+  ('bmw_x7.jpg', 2023, 'BMW X7', 'BMW', 'V8', '5.1 seconds', 22, 'Gasoline', '7 seats', 7, '350 miles', 240.00),
+  ('audi_q3.jpg', 2023, 'Audi Q3', 'Audi', 'Inline-4', '7.1 seconds', 33, 'Gasoline', '5 seats', 5, '380 miles', 210.00),
+  ('lexus_rx.jpg', 2023, 'Lexus RX', 'Lexus', 'V6', '7.0 seconds', 29, 'Gasoline', '5 seats', 5, '400 miles', 220.00),
+  ('rolls_royce_phantom.jpg', 2023, 'Rolls-Royce Phantom', 'Rolls-Royce', 'V12', '4.3 seconds', 15, 'Gasoline', '5 seats', 5, '350 miles', 500.00),
+  ('ferrari_enzo.jpg', 2023, 'Ferrari Enzo', 'Ferrari', 'V12', '3.1 seconds', 14, 'Gasoline', '2 seats', 2, '240 miles', 600.00),
+  ('aston_martin_db9.jpg', 2023, 'Aston Martin DB9', 'Aston Martin', 'V12', '4.1 seconds', 18, 'Gasoline', '2 seats', 2, '300 miles', 450.00),
+  ('rolls_royce_cullinan_black_badge.jpg', 2023, 'Rolls-Royce Cullinan Black Badge', 'Rolls-Royce', 'V12', '4.7 seconds', 13, 'Gasoline', '5 seats', 5, '320 miles', 600.00),
+  ('ferrari_la_ferrari.jpg', 2023, 'Ferrari LaFerrari', 'Ferrari', 'V12', '2.4 seconds', 13, 'Hybrid', '2 seats', 2, '200 miles', 1200.00),
+  ('lamborghini_centenario.jpg', 2023, 'Lamborghini Centenario', 'Lamborghini', 'V12', '2.8 seconds', 14, 'Gasoline', '2 seats', 2, '230 miles', 1000.00),
+  ('pagani_zonda.jpg', 2023, 'Pagani Zonda', 'Pagani', 'V12', '3.3 seconds', 11, 'Gasoline', '2 seats', 2, '220 miles', 900.00),
+  ('mclaren_675lt.jpg', 2023, 'McLaren 675LT', 'McLaren', 'V8', '2.9 seconds', 17, 'Gasoline', '2 seats', 2, '240 miles', 800.00),
+  ('aston_martin_one_77.jpg', 2023, 'Aston Martin One-77', 'Aston Martin', 'V12', '3.4 seconds', 15, 'Gasoline', '2 seats', 2, '250 miles', 700.00),
+  ('koenigsegg_jesko.jpg', 2023, 'Koenigsegg Jesko', 'Koenigsegg', 'V8', '2.6 seconds', 11, 'Gasoline', '2 seats', 2, '210 miles', 1000.00),
+  ('pagani_huayra_roadster.jpg', 2023, 'Pagani Huayra Roadster', 'Pagani', 'V12', '2.9 seconds', 12, 'Gasoline', '2 seats', 2, '230 miles', 900.00),
+  ('lamborghini_huracan_evo.jpg', 2023, 'Lamborghini Huracan Evo', 'Lamborghini', 'V10', '2.9 seconds', 14, 'Gasoline', '2 seats', 2, '230 miles', 700.00),
+  ('audi_e-tron_gt.jpg', 2023, 'Audi e-tron GT', 'Audi', 'Electric', '3.5 seconds', 90, 'Electric', '5 seats', 5, '250 miles', 700.00);
