61a3bccf99ec4fb76a167fb96f64c8f4d311ce0c
BVonan <bohnny14@gmail.com>
2023-12-03 20:44:35 -0800

    replaced the team page with tailwind instead of css

--- ourTeam.php
+++ ourTeam.php
@@ -10,37 +10,34 @@ session_start(); // Initialize the session
     <title>Our Team</title>
 </head>
 
-<body>
-<?php include 'includes/header.php'; ?>
+<body class="bg-gray-100">
+    <?php include 'includes/header.php'; ?>
 
-    <div class="container py-4">
-        <div class="row">
-            <div class="col-md-8 offset-md-2">
-                <h2 class="text-center mb-4">As the sole member of our team...</h2>
-                <div class="card">
-                    <div class="card-body">
-                        <p class="card-text">
-                            As the sole member of our team, I handle all aspects of our business, from
-                            conceptualization to development. Here are some of my responsibilities:
-                        </p>
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item">Conceptualizing and designing user interfaces</li>
-                            <li class="list-group-item">Programming and developing the website</li>
-                            <li class="list-group-item">Managing business operations</li>
-                            <li class="list-group-item">Ensuring customer satisfaction</li>
-                        </ul>
-                        <p class="card-text mt-4">
-                            I am passionate about delivering high-quality products and services to our
-                            users and continuously improving our offerings.
-                        </p>
-                        <!-- You can add your photo or any relevant images here -->
-                    </div>
-                </div>
+    <div class="container mx-auto px-4 py-8">
+        <div class="max-w-3xl mx-auto">
+            <h2 class="text-center text-3xl font-bold mb-4 text-blue-500">As the sole member of our team...</h2>
+            <div class="bg-white rounded-lg shadow-md p-6">
+                <p class="mb-4">
+                    As the sole member of our team, I handle all aspects of our business, from
+                    conceptualization to development. Here are some of my responsibilities:
+                </p>
+                <ul class="list-disc list-inside mb-6">
+                    <li>Conceptualizing and designing user interfaces</li>
+                    <li>Programming and developing the website</li>
+                    <li>Managing business operations</li>
+                    <li>Ensuring customer satisfaction</li>
+                </ul>
+                <p class="mt-4">
+                    I am passionate about delivering high-quality products and services to our
+                    users and continuously improving our offerings.
+                </p>
+                <!-- You can add your photo or any relevant images here -->
             </div>
         </div>
     </div>
-    <?php include 'includes/footer.php'; ?>
 
+    <?php include 'includes/footer.php'; ?>
 </body>
+
 
 </html>
