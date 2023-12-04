e9ec06b5540ea14f76e893c5166ef665cb0a0310
BVonan <bohnny14@gmail.com>
2023-12-03 21:34:49 -0800

    updated the login

--- login.php
+++ login.php
@@ -47,50 +47,40 @@ $conn->close();
 
 <head>
     <title>Log In</title>
+    <?php include 'includes/header.php'; ?>
 </head>
 
-<?php include 'includes/header.php'; ?>
-
-<body>
-    <section class="vh-100 gradient-custom">
-        <div>
-            <div class="row d-flex justify-content-center align-items-center vh-100">
-                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-                    <div class="card text-center">
-                        <div class="card-body p-5">
-                            <h2 class="card-title fw-bold mb-4 text-uppercase">Log In</h2>
-                            <?php
-                            if (isset($loginError)) {
-                                echo '<p class="text-danger">' . $loginError . '</p>';
-                            }
-                            ?>
-                            <!-- Disclaimer for non-logged-in users -->
-                            <p class="text">You must be logged in to make a testimonial.</p>
-                            <br>
-                            <form action="login.php" method="post">
-                                <div class="mb-4">
-                                    <label for="username" class="form-label">Username:</label>
-                                    <input type="text" id="username" class="form-control form-control-lg"
-                                        name="username" required>
-                                </div>
-                                <div class="mb-4">
-                                    <label for="password" class="form-label">Password:</label>
-                                    <input type="password" id="password" class="form-control form-control-lg"
-                                        name="password" required>
-                                </div>
-                                <button class="btn btn-primary btn-lg px-5" type="submit">Log In</button>
-                            </form>
-                            <div class="mt-4">
-                                <p class="mb-0">Don't have an account? <a href="#!" class="fw-bold">Sign Up</a></p>
-                            </div>
-                        </div>
-                    </div>
+<body class="bg-gray-100">
+    <section class=" vh-100 gradient-custom min-h-screen flex items-center justify-center">
+        <div class="max-w-md w-full mx-auto p-6 bg-white rounded-lg shadow-md">
+            <h2 class="text-3xl font-bold mb-4">Log In</h2>
+            <?php
+            if (isset($loginError)) {
+                echo '<p class="text-red-500">' . $loginError . '</p>';
+            }
+            ?>
+            <!-- Disclaimer for non-logged-in users -->
+            <p class="mb-4">You must be logged in to make a testimonial.</p>
+            <form action="login.php" method="post" class="space-y-4">
+                <div>
+                    <label for="username" class="block font-medium">Username:</label>
+                    <input type="text" id="username" class="w-full border rounded-lg px-3 py-2" name="username"
+                        required>
                 </div>
+                <div>
+                    <label for="password" class="block font-medium">Password:</label>
+                    <input type="password" id="password" class="w-full border rounded-lg px-3 py-2" name="password"
+                        required>
+                </div>
+                <button class="bg-blue-500 text-white rounded-lg px-5 py-2 hover:bg-blue-600 transition-all"
+                    type="submit">Log In</button>
+            </form>
+            <div class="mt-4 text-center">
+                <p>Don't have an account? <a href="signup.php" class="font-semibold text-blue-500">Sign Up</a></p>
             </div>
         </div>
     </section>
     <?php include 'includes/footer.php'; ?>
-
 </body>
 
 </html>
