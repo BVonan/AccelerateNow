6fe7fd1ab360e91837af7bb01e4b6e73ea0d944a
BVonan <bohnny14@gmail.com>
2023-12-17 22:06:10 -0800

    uploaded the chiron image

--- assets/config/imageuploader.php
+++ assets/config/imageuploader.php
@@ -6,7 +6,7 @@ $password = "bohnnY06!";
 $dbname = "AccelerateNow";
 
 // Image file path
-$imagePath = '/Users/bohnny/Herd/AccelerateNow/images/LAMBORGHINI_STO-54.jpeg';
+$imagePath = '/Users/bohnny/Herd/AccelerateNow/images/chiron.jpeg';
 
 // Read the image into a variable
 $imageData = file_get_contents($imagePath);
@@ -24,7 +24,7 @@ $stmt = $conn->prepare("UPDATE cars SET image = ? WHERE car_id = ?");
 $stmt->bind_param("si", $imageData, $carId); // Assuming 'image' column is of type longblob
 
 // Car ID for which you want to update the image
-$carId = 9;
+$carId = 46;
 
 // Execute the update
 if ($stmt->execute()) {
